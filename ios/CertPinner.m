779e0e78df48bc942ff06d2378d942c217299aa6
Robert Kong <robert.kong@ge.com>
2019-01-19 13:53:10 -0800

    Update logic to include ability to enable/disable pinning using SVMX TrustKit fork

--- ios/CertPinner.m
+++ ios/CertPinner.m
@@ -1,5 +1,7 @@
 
 #import "CertPinner.h"
+#import <React/RCTLog.h>
+#import "TrustKit/TrustKit.h"
 
 @implementation CertPinner
 
@@ -8,6 +10,13 @@
     return dispatch_get_main_queue();
 }
 RCT_EXPORT_MODULE()
+
+RCT_EXPORT_METHOD(enableCertPinning:(BOOL)enable)
+{
+    // Here's our method's code
+    [[TrustKit sharedInstance] pinningValidator].enforcePinning = enable;
+    RCTLogInfo(@"enableCertPinning called with %d!", enable);
+}
 
 @end
   
